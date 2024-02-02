a03e5ece7bc0d14c0507ea4207cd745c6670b19a
OlgaSkirda <137907769+OlgaSkirda@users.noreply.github.com>
2024-02-02 18:11:37 +0300

    Add files via upload

---
+++ project_2/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        double floatVeriable;
+        floatVeriable = 10.2;
+
+        byte byteVariable;
+        byteVariable = 127;
+    }
+}
